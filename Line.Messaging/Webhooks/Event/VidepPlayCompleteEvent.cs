bc48e8ab974e9ebdac900c70c61351da4cc13ff8
kaminaka rikuya <rikupin1105@gmail.com>
2021-01-24 16:23:22 +0900

    Create VidepPlayCompleteEvent.cs

---
+++ Line.Messaging/Webhooks/Event/VidepPlayCompleteEvent.cs
@@ -0,0 +1,20 @@
+﻿namespace Line.Messaging.Webhooks
+{
+    public class VideoPlayCompleteEvent : ReplyableEvent
+    {
+        public VideoPlayComplete VideoPlayComplete;
+
+        public VideoPlayCompleteEvent(WebhookEventSource source, long timestamp, string mode, string replyToken, VideoPlayComplete videoPlayComplete) : base(WebhookEventType.VideoPlayComplete, source, timestamp, mode, replyToken)
+        {
+            VideoPlayComplete = videoPlayComplete;
+        }
+    }
+    public class VideoPlayComplete
+    {
+        public VideoPlayComplete(string trackingId)
+        {
+            TrackingId = trackingId;
+        }
+        public string TrackingId { get; }
+    }
+}
